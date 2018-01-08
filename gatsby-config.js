bb3dbd8ea6a2836f3207a059ac9083e80781b44a
amandeepmittal <amandeepmittal@live.com>
2018-01-08 13:30:58 +0530

    add config and assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,35 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: "Gatsby Starter - Ceevee by Styleshout",
+    author: "Aman Mittal",
+    description: "A Gatsby.js Starter based on Ceevee by Styleshout"
   },
-  plugins: [`gatsby-plugin-react-helmet`],
-}
+  pathPrefix: "/",
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`
+      }
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630
+            }
+          },
+          "gatsby-remark-copy-linked-files"
+        ]
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`
+  ]
+};
