3d34af3ad8a17f5004c3f98db0b92f2090df0823
Toni Ristola <toni.ristola@gofore.com>
2018-10-08 16:00:37 +0300

    show current anchor

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,6 @@ const isCurrent = (anchor, pathname) => (
   ? 'current'
   : ''
 )
-
 class Navigation extends React.Component {
   state = {
     pathname: '#home'
@@ -16,36 +15,37 @@ class Navigation extends React.Component {
     })
   }
   render() {
+    const { pathname = '' } = this.state
     return (
     <nav id="nav-wrap">
       <a className="mobile-btn" href="#nav-wrap" />
       <ul id="nav" className="nav">
-        <li className="current">
+        <li className={isCurrent('#home', pathname)}>
           <a className="smoothscroll" href="#home">
             Home
           </a>
         </li>
-        <li>
+        <li className={isCurrent('#about', pathname)}>
           <a className="smoothscroll" href="#about">
             About
           </a>
         </li>
-        <li>
+        <li className={isCurrent('#resume', pathname)}>
           <a className="smoothscroll" href="#resume">
             Resume
           </a>
         </li>
-        <li>
+        <li className={isCurrent('#portfolio', pathname)}>
           <a className="smoothscroll" href="#portfolio">
             Works
           </a>
         </li>
-        <li>
+        <li className={isCurrent('#testimonials', pathname)}>
           <a className="smoothscroll" href="#testimonials">
             Testimonials
           </a>
         </li>
-        <li>
+        <li className={isCurrent('#footer', pathname)}>
           <a className="smoothscroll" href="#footer">
             Footer
           </a>
